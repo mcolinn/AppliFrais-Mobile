5a785137a0f3f077abbcbb56f73e37bd6402ecf8
m.colin <m.colin@sciencesu-lyon.fr>
2017-04-06 15:00:00 +0200

    Ajout de tout les fichiers du projet

---
+++ app/src/androidTest/java/ppe_matthieu/myapplifraismobile/LoginActivityTest.java
@@ -0,0 +1,34 @@
+package ppe_matthieu.myapplifraismobile;
+
+import org.junit.Test;
+
+/**
+ * Created by Matthieu_2 on 30/03/2017.
+ */
+public class LoginActivityTest extends AddFraisActivity {
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+    @Test
+    public void onClick() throws Exception {
+
+    }
+
+    @Test
+    public void onCreateLoader() throws Exception {
+
+    }
+
+    @Test
+    public void onLoadFinished() throws Exception {
+
+    }
+
+    @Test
+    public void onLoaderReset() throws Exception {
+
+    }
+
+}
